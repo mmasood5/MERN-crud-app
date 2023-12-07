1785654de3fabf367eba483c0204cbdf4e2bb442
MinhajM <44679844+mmasood5@users.noreply.github.com>
2023-12-07 04:06:58 -0500

    Created landing page, form, syling

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './components/navbar';
+//pages & components
+import Home from './pages/home'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className='pages'>
+          <Routes>
+            <Route 
+              path = "/"
+              element = {<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
